0f7c7d7c0e2a66e37d64e98f88dbc1db223400cf
Ezekiel <ezekieljamolin186@gmail.com>
2023-10-08 21:36:30 -0700

    worked on the css

--- script.js
+++ script.js
@@ -12,26 +12,32 @@ var date2 = document.querySelector(".date2")
 var wind2text = document.querySelector(".wind2")
 var temp2text = document.querySelector(".temp2")
 var hum2text = document.querySelector(".hum2")
+var image2 = document.getElementById("image2")
 
 var date3 = document.querySelector(".date3")
 var wind3text = document.querySelector(".wind3")
 var temp3text = document.querySelector(".temp3")
 var hum3text = document.querySelector(".hum3")
+var image3 = document.getElementById("image3")
 
 var date4 = document.querySelector(".date4")
 var wind4text = document.querySelector(".wind4")
 var temp4text = document.querySelector(".temp4")
 var hum4text = document.querySelector(".hum4")
+var image4 = document.getElementById("image4")
 
 var date5 = document.querySelector(".date5")
 var wind5text = document.querySelector(".wind5")
 var temp5text = document.querySelector(".temp5")
 var hum5text = document.querySelector(".hum5")
+var image5 = document.getElementById("image5")
 
 var date6 = document.querySelector(".date6")
 var wind6text = document.querySelector(".wind6")
 var temp6text = document.querySelector(".temp6")
 var hum6text = document.querySelector(".hum6")
+var image6 = document.getElementById("image6")
+
 // var button = document.querySelector(".btn1")
 // var cityDisplay = document.querySelector("#city");
 // var forecastContainter = document.querySelector(".forecast-container");
@@ -119,6 +125,10 @@ fetch(api)
     hum2text.textContent = hum2 + "%"
     var temp2 = data.list[7].main.temp;
     temp2text.textContent = ((temp2 - 273.15) * 9/5 + 32).toFixed(2) + " °F"
+    var iconCode2 = data.list[7].weather[0].icon
+    console.log(iconCode2)
+    var icon = "http://openweathermap.org/img/wn/" + iconCode2 + ".png"
+    image2.src = icon
 
     var apiDate3 = data.list[15].dt_txt;
     console.log(apiDate3);
@@ -131,6 +141,10 @@ fetch(api)
     hum3text.textContent = hum3 + "%"
     var temp3 = data.list[15].main.temp;
     temp3text.textContent = ((temp3 - 273.15) * 9/5 + 32).toFixed(2) + " °F"
+    var iconCode3 = data.list[15].weather[0].icon
+    console.log(iconCode3)
+    var icon = "http://openweathermap.org/img/wn/" + iconCode3 + ".png"
+    image3.src = icon
 
     var apiDate4 = data.list[23].dt_txt;
     console.log(apiDate4);
@@ -143,6 +157,10 @@ fetch(api)
     hum4text.textContent = hum4 + "%"
     var temp4 = data.list[23].main.temp;
     temp4text.textContent = ((temp4 - 273.15) * 9/5 + 32).toFixed(2) + " °F"
+    var iconCode4 = data.list[23].weather[0].icon
+    console.log(iconCode4)
+    var icon = "http://openweathermap.org/img/wn/" + iconCode4 + ".png"
+    image4.src = icon
 
     var apiDate5 = data.list[31].dt_txt;
     console.log(apiDate5);
@@ -155,6 +173,10 @@ fetch(api)
     hum5text.textContent = hum5 + "%"
     var temp5 = data.list[31].main.temp;
     temp5text.textContent = ((temp5 - 273.15) * 9/5 + 32).toFixed(2) + " °F"
+    var iconCode5 = data.list[31].weather[0].icon
+    console.log(iconCode5)
+    var icon = "http://openweathermap.org/img/wn/" + iconCode5 + ".png"
+    image5.src = icon
 
     var apiDate6 = data.list[39].dt_txt;
     console.log(apiDate6);
@@ -167,5 +189,9 @@ fetch(api)
     hum6text.textContent = hum6 + "%"
     var temp6 = data.list[39].main.temp;
     temp6text.textContent = ((temp6 - 273.15) * 9/5 + 32).toFixed(2) + " °F"
+    var iconCode6 = data.list[39].weather[0].icon
+    console.log(iconCode6)
+    var icon = "http://openweathermap.org/img/wn/" + iconCode6 + ".png"
+    image6.src = icon
   })
 }
